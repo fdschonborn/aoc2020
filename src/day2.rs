603d5af1d9985215c2c8b07ebca20a0c487c5f38
Federico Damian Schonborn <fdschonborn@gmail.com>
2020-12-02 17:33:45 -0300

    Day 2 Part 1
    This is so convoluted I am going to die
    
    Signed-off-by: Federico Damian Schonborn <fdschonborn@gmail.com>

---
+++ src/day2.rs
@@ -0,0 +1,55 @@
+use std::str::FromStr;
+
+use once_cell::sync::Lazy;
+use thiserror::Error;
+
+#[derive(Debug)]
+pub struct Entry {
+    pub min: usize,
+    pub max: usize,
+    pub letter: char,
+    pub pass: String,
+}
+
+#[derive(Debug, Error)]
+#[error("failed to parse entry")]
+pub struct ParseEntryError;
+
+impl FromStr for Entry {
+    type Err = ParseEntryError;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let split = input
+            .split(&['-', ' ', ':'][..]) // Rust wtf.
+            .filter(|sub| !sub.is_empty())
+            .collect::<Vec<_>>();
+
+        Ok(Entry {
+            min: split[0].parse().unwrap(),
+            max: split[1].parse().unwrap(),
+            letter: split[2].parse().unwrap(),
+            pass: split[3].parse().unwrap(),
+        })
+    }
+}
+
+pub static DATABASE: Lazy<Vec<Entry>> = Lazy::new(|| {
+    include_str!("../data/day2.txt")
+        .lines()
+        .map(|line| line.parse::<Entry>().expect("Failed to parse entry"))
+        .collect::<Vec<_>>()
+});
+
+#[test]
+fn test() {
+    println!(
+        "Count of Valid Passwords: {}",
+        DATABASE
+            .iter()
+            .filter(|entry| {
+                (entry.min..=entry.max)
+                    .contains(&entry.pass.chars().filter(|c| *c == entry.letter).count())
+            })
+            .count()
+    );
+}
