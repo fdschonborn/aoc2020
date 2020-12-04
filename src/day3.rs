38cbb067e062e58be1f4733737fe9b3060028e9c
Federico Damian Schonborn <fdschonborn@gmail.com>
2020-12-03 22:55:30 -0300

    Update names, that's all
    Signed-off-by: Federico Damian Schonborn <fdschonborn@gmail.com>

--- src/day3.rs
+++ src/day3.rs
@@ -1,12 +1,12 @@
-fn count_trees(input: &str, right: usize, down: usize) -> usize {
+pub fn count_trees(input: &str, right: usize, down: usize) -> usize {
     let mut offset = 0;
 
     input
         .lines()
         .enumerate()
-        .filter(|(line, _)| if down > 0 { line % down == 0 } else { true })
-        .filter(|(_, text)| {
-            let matches = text.chars().cycle().nth(offset).unwrap() == '#';
+        .filter(|(n, _)| if down > 0 { n % down == 0 } else { true })
+        .filter(|(_, line)| {
+            let matches = line.chars().cycle().nth(offset).unwrap() == '#';
             offset += right;
 
             matches
@@ -15,12 +15,12 @@ fn count_trees(input: &str, right: usize, down: usize) -> usize {
 }
 
 #[aoc(day3, part1)]
-fn part1(input: &str) -> usize {
+pub fn day3_part1(input: &str) -> usize {
     count_trees(input, 3, 1)
 }
 
 #[aoc(day3, part2)]
-fn part2(input: &str) -> usize {
+pub fn day3_part2(input: &str) -> usize {
     count_trees(input, 1, 1)
         * count_trees(input, 3, 1)
         * count_trees(input, 5, 1)
