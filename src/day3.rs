a2b3df4ad1ac5dfdc7969bbbb697123798838d5b
Federico Damian Schonborn <fdschonborn@gmail.com>
2020-12-03 22:24:30 -0300

    Day 3 Part 2
    Signed-off-by: Federico Damian Schonborn <fdschonborn@gmail.com>

--- src/day3.rs
+++ src/day3.rs
@@ -1,14 +1,31 @@
-#[aoc(day3, part1)]
-fn part1(input: &str) -> usize {
+fn count_trees(input: &str, right: usize, down: usize) -> usize {
     let mut offset = 0;
 
     input
         .lines()
-        .filter(|text| {
+        .enumerate()
+        .filter(|(line, _)| if down > 0 { line % down == 0 } else { true })
+        .filter(|(_, text)| {
             let matches = text.chars().cycle().nth(offset).unwrap() == '#';
-            offset += 3;
+            offset += right;
 
             matches
         })
         .count()
+}
+
+#[aoc(day3, part1)]
+fn part1(input: &str) -> usize {
+    count_trees(input, 3, 1)
+}
+
+#[aoc(day3, part2)]
+fn part2(input: &str) -> usize {
+    let a = dbg!(count_trees(input, 1, 1));
+    let b = dbg!(count_trees(input, 3, 1));
+    let c = dbg!(count_trees(input, 5, 1));
+    let d = dbg!(count_trees(input, 7, 1));
+    let e = dbg!(count_trees(input, 1, 2));
+
+    a * b * c * d * e
 }
